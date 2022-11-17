1d38c1477ccd84c3b939822596f29a6305863c15
unknown <pandeybhuwan138@gmail.com>
2022-11-17 12:31:51 +0545

    Connection Build to database

--- evoting/src/java/evoting/dbutil/DBConnection.java
+++ evoting/src/java/evoting/dbutil/DBConnection.java
@@ -4,10 +4,43 @@
  */
 package evoting.dbutil;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author Bhuwan Pandey
  */
 public class DBConnection {
+    private static Connection conn = null;
     
+    static{
+        try{
+            //loading driver
+            Class.forName("oracle.jdbc.OracleDriver");
+            
+            //getting driver connection
+            String url = "jdbc:oracle:thin:@//localhost:1521/xe";
+            String userName = "evoting_db";
+            String password = "evoting";
+            conn = DriverManager.getConnection(url,userName,password);
+        }catch(ClassNotFoundException | SQLException exception){
+            exception.printStackTrace();
+        }
+    }
+    
+    public static Connection getConnection(){
+        return conn;
+    }
+    
+    public static void closeConnection(){
+        try{
+            if(conn != null){
+                conn.close();
+            }
+        }catch(SQLException exception){
+            exception.printStackTrace();
+        }
+    }
 }
